9dc7f4752002b40bef55b37603db01774da33f21
hosan30 <50867996+ahfoysal@users.noreply.github.com>
2023-12-18 19:16:26 +0600

    Added authorization  to route

--- src/app/modules/course/course.route.js
+++ src/app/modules/course/course.route.js
@@ -2,26 +2,21 @@ const express = require('express')
 const CourseController = require('./course.controller')
 const validateRequest = require('../../middlewares/validateRequest')
 const CourseValidation = require('./course.validate')
+const { ENUM_USER_ROLE } = require('../../../enums/user')
+const auth = require('../../middlewares/auth')
 
 const router = express.Router()
 
 router.post(
   '/',
   validateRequest(CourseValidation.createCourse),
+  auth(ENUM_USER_ROLE.ADMIN),
   CourseController.createCourse,
 )
 
 router.get('/', CourseController.getAllCourse)
 router.get('/:id', CourseController.getSingleCourse)
-router.patch(
-  '/:id',
-
-  CourseController.updateCourse,
-)
-router.delete(
-  '/:id',
-
-  CourseController.deleteCourse,
-)
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), CourseController.updateCourse)
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), CourseController.deleteCourse)
 
 exports.CourseRoutes = router
